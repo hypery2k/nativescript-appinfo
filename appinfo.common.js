1d60a72cc3444bfd45ea005c4e72e748ba672211
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-05-22 17:29:02 +0200

    feat(typescript): Update to typescript 2.3

--- appinfo.common.js
+++ appinfo.common.js
@@ -1,7 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var platform_details_1 = require("./platform-details");
 function getPlatformDetails() {
     return new platform_details_1.PlatformDetails();
 }
 exports.getPlatformDetails = getPlatformDetails;
 ;
+//# sourceMappingURL=appinfo.common.js.map
